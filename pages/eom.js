ddf4020a59e51e207ee0d49869cf3d43909c62ed
SemihOzden <semih.ozden@bumper.co.uk>
2021-10-30 02:26:40 +0300

    image updated

--- pages/eom.js
+++ pages/eom.js
@@ -1,36 +1,62 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
-import styles from "../styles/EOM.module.css"
-import { Toolbar } from '../components/toolbar';
+import styles from "../styles/EOM.module.css";
+import { Toolbar } from "../components/toolbar";
+import Head from "next/head";
 
-export const EOM= ({employee})=> {
-    console.log(employee.image)
-    return (
-        <div className="page-container">
-            <Toolbar/>
+export const EOM = ({ employee }) => {
+  console.log(employee.image);
+  return (
+    <>
+      <Head>
+        <title>Employee Of The Month</title>
+        <meta
+          name="description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
 
-            <div className={styles.main}>
-                <h1>Employee of the Month</h1>
-                <div className={styles.employeeOfTheMonth}>
-                    <h3>{employee.name}</h3>
-                    <h6>{employee.position}</h6>
-                    <img src={employee.image} alt=""/>
-                    <p>{employee.description}</p>
-                </div>
-            </div>
+        <meta property="og:image" content={employee.image} />
+        <meta property="og:title" content="Employee Of The Month" />
+        <meta
+          property="og:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+
+        <meta property="twitter:image" content={employee.image} />
+        <meta property="twitter:title" content="Employee Of The Month" />
+        <meta
+          property="twitter:description"
+          content={`This month's employee of the month is ${employee.name}`}
+        />
+      </Head>
+      <div className="page-container">
+        <Toolbar />
+
+        <div className={styles.main}>
+          <h1>Employee of the Month</h1>
+          <div className={styles.employeeOfTheMonth}>
+            <h3>{employee.name}</h3>
+            <h6>{employee.position}</h6>
+            <img src={"https://images.pexels.com/photos/1461974/pexels-photo-1461974.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"} alt="" />
+            <p>{employee.description}</p>
+          </div>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export const getServerSideProps= async pageContext=>{
-    const apiResponse= await fetch('https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth');
-    const employee= await apiResponse.json();
+export const getServerSideProps = async (pageContext) => {
+  const apiResponse = await fetch(
+    "https://my-json-server.typicode.com/portexe/next-news/employeeOfTheMonth"
+  );
+  const employee = await apiResponse.json();
 
-    return {
-        props:{
-            employee:employee
-        }
-    }
-}
+  return {
+    props: {
+      employee: employee,
+    },
+  };
+};
 
 export default EOM;
--- pages/feed/[slug].js
+++ pages/feed/[slug].js
@@ -1,50 +1,60 @@
 import styles from "../../styles/Feed.module.css";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 import { Toolbar } from "../../components/toolbar";
+import Head from "next/head";
 
 export const Feed = ({ pageNumber, articles }) => {
-    const router = useRouter();
+  const router = useRouter();
 
   return (
-    <div className="page-container">
-        <Toolbar/>
-      <div className={styles.main}>
-        {articles.map((article, index) => (
-          <div key={index} className={styles.post}>
-            <h1 onClick={() => (window.location.href = article.url)}>
-              {article.title}
-            </h1>
-            <p>{article.description}</p>
-            {!!article.urlToImage && <img src={article.urlToImage} alt="" />}
-          </div>
-        ))}
-      </div>
-      <div className={styles.paginator}>
+    <>
+      <Head>
+        <meta property="og:image" content={articles[0]?.urlToImage} />
+        <meta property="og:description" content={articles[0]?.description} />
+        <meta property="og:title" content={articles[0]?.title + " and more!"} />
+      </Head>
+      <div className="page-container">
+        <Toolbar />
+        <div className={styles.main}>
+          {articles.map((article, index) => (
+            <div key={index} className={styles.post}>
+              <h1 onClick={() => (window.location.href = article.url)}>
+                {article.title}
+              </h1>
+              <p>{article.description}</p>
+              {!!article.urlToImage && <img src={article.urlToImage} alt="" />}
+            </div>
+          ))}
+        </div>
+        <div className={styles.paginator}>
           <div
-          onClick={()=>{
-            if(pageNumber > 1){
-                  router.push(`/feed/${pageNumber-1}`).then(()=>{window.scrollTo(0,0)})
-            }
-          }
-          }
-           className={pageNumber===1 ? styles.disabled : styles.active}>
-               Previous Page
-           </div>
-           <div>
-               #{pageNumber}
-           </div>
-           <div
-          onClick={()=>{
-            if(pageNumber < 5){
-                  router.push(`/feed/${pageNumber+1}`).then(()=>{window.scrollTo(0,0)})
-            }
-          }
-          }
-           className={pageNumber===5 ? styles.disabled : styles.active}>
-               Next Page
-           </div>
+            onClick={() => {
+              if (pageNumber > 1) {
+                router.push(`/feed/${pageNumber - 1}`).then(() => {
+                  window.scrollTo(0, 0);
+                });
+              }
+            }}
+            className={pageNumber === 1 ? styles.disabled : styles.active}
+          >
+            Previous Page
+          </div>
+          <div>#{pageNumber}</div>
+          <div
+            onClick={() => {
+              if (pageNumber < 5) {
+                router.push(`/feed/${pageNumber + 1}`).then(() => {
+                  window.scrollTo(0, 0);
+                });
+              }
+            }}
+            className={pageNumber === 5 ? styles.disabled : styles.active}
+          >
+            Next Page
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
